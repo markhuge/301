ffbbd9b7bb87075a9aff36e74d0615649a2c1cca
markhuge <mark@segfawlt.net>
2015-09-03 05:10:53 -0700

    added version flag

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ This is a Go port of a [project I built with Node.js a while back](https://githu
 A super simple HTTP redirection server
 
 -h, --help    print this message
+-v, --version print 301 version
 -p, --port    port 301 will listen on (default 8080)
 -d, --domain  domain requests will redirect to (default 127.0.0.1)
     --proto   protocol HTTP or HTTPS (default HTTP)
--- cli.go
+++ cli.go
@@ -8,10 +8,12 @@ import (
 
 var port, health int
 var domain, proto string
+var version = "v2.0.0"
 var usage = `
 A super simple HTTP redirection server
 
 -h, --help    print this message
+-v, --version print 301 version
 -p, --port    port 301 will listen on (default 8080)
 -d, --domain  domain requests will redirect to (default 127.0.0.1)
     --proto   protocol HTTP or HTTPS (default HTTP)
@@ -25,6 +27,11 @@ func handleCLI() (proto, domain string, port, health int) {
 
 	if args.MayBool(false, "help", "h", "?") {
 		fmt.Println(usage)
+		os.Exit(0)
+	}
+
+	if args.MayBool(false, "v", "version") {
+		fmt.Println(version)
 		os.Exit(0)
 	}
 
